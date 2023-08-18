54447a3b8c10dca4c88fa6e36b98c71eacf6a4d7
aluno <Aluno@PR02-302-25.CSED.LAB>
2023-08-18 11:31:46 -0300

    Primeiro commit

---
+++ Ambientacao/Ambientacao/Program.cs
@@ -0,0 +1,59 @@
+﻿int tamanho = 10;
+int[] vetor = new int[tamanho];
+int aux = 0;
+Boolean troca = false;
+Random random = new Random();
+
+
+
+Console.Clear();
+
+for (int i = 0; i < tamanho; i++)
+{
+    vetor[i] = random.Next(0, 500);
+}
+
+// for (int j = 0; j < tamanho; j++)
+// {    
+//     for (int i = 0; i < tamanho - 1; i++)
+//     {
+        
+//         if(vetor[i] > vetor[i+1]){
+//             aux = vetor[i];
+//             vetor[i] = vetor[i+1];
+//             vetor[i+1] = aux;
+//         }
+//     }
+// }
+
+do
+{
+    troca = false;
+    for (int i = 0; i < tamanho - 1; i++)
+    {
+        if(vetor[i] > vetor[i+1]){
+            aux = vetor[i];
+            vetor[i] = vetor[i+1];
+            vetor[i+1] = aux;
+            troca = true;
+        }
+    }
+}
+while(troca);
+
+
+
+
+void ImpremirVetor()
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        Console.Write(vetor[i] + " ");
+    }
+    Console.WriteLine("\n");
+}
+
+//Array.Sort(vetor);
+
+
+ImpremirVetor();
---
+++ Ambientacao/OrientacaoObjetos/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace OrientacaoObjetos.Models;
+public class Pessoa
+{
+    private String nome;
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+}
